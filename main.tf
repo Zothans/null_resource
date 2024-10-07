0d128b7f8962ac84ac7d2645f263df8e672c543a
Zsanga <105400251+Zothans@users.noreply.github.com>
2024-10-07 14:56:10 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,52 @@ terraform {
       source = "hashicorp/null"
       version = "3.2.1"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    databricks = {
+      source = "databricks/databricks"
+      version = "1.52.0"
+    }
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.4.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.32.0"
+    }
+
+    google = {
+      source = "hashicorp/google"
+      version = "6.5.0"
+    }
   }
 }
 
 provider "null" {
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+provider "azurerm" {
+  # Configuration options
+}
+
+provider "databricks" {
+  # Configuration options
+}
+
+provider "kubernetes" {
+  # Configuration options
+}
+
+provider "google" {
+  # Configuration options
 }
 
 resource "null_resource" "test" {}
