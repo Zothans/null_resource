c414a0770edce7aa20dcf30d269eeffced08b7e5
Zsanga <105400251+Zothans@users.noreply.github.com>
2025-03-17 12:16:42 +1100

    17 march 2025

--- main.tf
+++ main.tf
@@ -54,6 +54,6 @@ provider "google" {
 
 resource "null_resource" "test" {}
 resource "null_resource" "test1" {}
-resource "null_resource" "thisistest" {}
+resource "null_resource" "idamem" {}
 
 #added comments
