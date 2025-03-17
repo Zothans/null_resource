79999121d56f7baca78cb1c24d67babb85ab3b8b
Zsanga <105400251+Zothans@users.noreply.github.com>
2025-03-17 14:45:48 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -54,6 +54,6 @@ provider "google" {
 
 resource "null_resource" "test" {}
 resource "null_resource" "test1" {}
-resource "null_resource" "hello" {}
+resource "null_resource" "sanga" {}
 
 #added comments
